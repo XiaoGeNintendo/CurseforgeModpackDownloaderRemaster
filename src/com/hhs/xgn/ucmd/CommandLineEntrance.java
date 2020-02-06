f6acf18fc23c06b35a2b6dc4bbb10a0bda38d65a
XiaoGeNintendo <34835642+XiaoGeNintendo@users.noreply.github.com>
2020-02-06 18:30:37 +0800

    use cdn

--- src/com/hhs/xgn/ucmd/CommandLineEntrance.java
+++ src/com/hhs/xgn/ucmd/CommandLineEntrance.java
@@ -4,11 +4,13 @@ import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -17,6 +19,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSyntaxException;
@@ -30,6 +33,17 @@ public class CommandLineEntrance {
 	ModpackManifest mm;
 	Gson gs;
     CloseableHttpClient httpClient = HttpClients.createDefault();
+    
+    String read(String url) throws MalformedURLException, IOException{
+    	InputStream is=new URL(url).openConnection().getInputStream();
+		BufferedReader br=new BufferedReader(new InputStreamReader(is, "utf-8"));
+		String s="",sl="";
+		while((sl=br.readLine())!=null){
+			s+=sl;
+		}
+		br.close();
+		return s;
+    }
     
 	void solve(String[] args) throws JsonSyntaxException, JsonIOException, UnsupportedEncodingException, FileNotFoundException{
 		if(args.length<2){
@@ -63,23 +77,30 @@ public class CommandLineEntrance {
 				
 				String url="https://ddph1n5l22.execute-api.eu-central-1.amazonaws.com/dev/mod/"+mf.projectID;
 				
-				InputStream is=new URL(url).openConnection().getInputStream();
-				BufferedReader br=new BufferedReader(new InputStreamReader(is, "utf-8"));
-				String s="",sl="";
-				while((sl=br.readLine())!=null){
-					s+=sl;
-				}
-				br.close();
+				String s=read(url);
 				
-				
-//				System.out.println("API Result String="+s);
-				//parse json
 				JsonObject jo=gs.fromJson(s, JsonObject.class);
-				String url2=jo.get("result").getAsJsonObject().get("url").getAsString();
+				String key=jo.get("result").getAsJsonObject().get("key").getAsString();
 				
-				System.out.println("Find project url="+url2);
-				String furl=url2+"/download/"+mf.fileID+"/file";
-				System.out.println("So file url="+furl);
+				System.out.println("Find project key="+key);
+				
+				String url2="https://ddph1n5l22.execute-api.eu-central-1.amazonaws.com/dev/mod/"+key+"/files";
+				
+				s=read(url2);
+				
+				jo=gs.fromJson(s, JsonObject.class);
+				JsonArray ja=jo.get("result").getAsJsonArray();
+				String furl=null;
+				for(int j=0;j<ja.size();j++){
+					JsonObject file=ja.get(j).getAsJsonObject();
+					if(file.get("id").getAsInt()==mf.fileID){
+						String name=file.get("file_name").getAsString();
+						System.out.println("Find name:"+name);
+						furl="https://media.forgecdn.net/files/"+mf.fileID/1000+"/"+mf.fileID%1000+"/"+name;
+						System.out.println("So, furl="+furl);
+						break;
+					}
+				}
 				pw.println(furl);
 				pw.flush();
 			}catch(Exception e){
